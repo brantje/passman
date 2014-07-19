04be9381d5ca14b7bf282a6dd442ff845c164a90
brantje <brantje@gmail.com>
2014-07-20 00:05:26 +0200

    Uodate fetching favicons

--- js/ocPassman.js
+++ js/ocPassman.js
@@ -790,9 +790,9 @@ function loadItems(){
 				 var favIcon = '<span class="icon-lock icon"></span>';
 				 if(url){
 					 var hostName = getLocation(url);
-					 hostName = hostName.protocol+'//'+hostName.hostname;
-					 var defaultIcon = location.protocol+'//'+location.hostname+OC.generateUrl('core/img/actions/lock.svg');
-					favIcon = '<img src="https://getfavicon.appspot.com/'+ hostName +'?defaulticon='+ defaultIcon +'" height="20" width="20" style="float: left; margin-left: 5px; margin-right: 4px; margin-top: 5px;">'
+					 hostName = encodeURIComponent(url);
+					 var defaultIcon = 'http://home.brantje.com/lock.svg'; //location.protocol+'//'+location.hostname+OC.generateUrl('core/img/actions/lock.svg');
+					favIcon = '<img src="https://getfavicon.appspot.com/'+ hostName +'?defaulticon='+ defaultIcon +'" style="height: 16px; width: 16px; float: left; margin-left: 8px; margin-right: 4px; margin-top: 5px;">';
 				 }
 			 	 var deleteIcon = (showingDeleted==0) ? '<i class="delete-icon icon" title="Delete" style="float: right; visibility: hidden;"></i>' : '<i class="icon-history icon" title="Recover" style="float: right; visibility: hidden;"></i>';
 				 var append = '<li data-id='+ this.id +'>'+ favIcon +'<div style="display: inline-block;" class="itemLabel">'+ this.label +'</div>'+ deleteIcon +''+ inlineTags  +'</li>';
