9b00ddf175b73e95c81513baed7c583194bde910
brantje <brantje@gmail.com>
2014-07-24 19:14:51 +0200

    Fix keepass import

--- js/importers/keepass2.js
+++ js/importers/keepass2.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 		var importer = [['Keepass 1.x', 'importKeepass2Dialog']];
 		$(document).data('importers', importer);
 	}
+	keepassData = '';
 })
-
 function processData(allText) {
 	var allTextLines = allText.split(/\r\n|\n/);
 	var headers = allTextLines[0].split(',');
@@ -29,15 +29,14 @@ function processData(allText) {
 	return lines;
 }
 
-function handleKeyPassFileChange(evt) {
+function handleFileChange(evt) {
 	//Retrieve the first (and only!) File from the FileList object
 	var f = evt.target.files[0];
 
 	if (f) {
 		var r = new FileReader();
 		r.onload = function(e) {
-			var contents = e.target.result;
-			keypassData = contents;
+			keepassData = e.target.result;
 		}
 		r.readAsText(f);
 	} else {
@@ -45,6 +44,30 @@ function handleKeyPassFileChange(evt) {
 	}
 }
 
+function importKeepass2Dialog() {
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+
+		$('<div id="keepassDialog"><p><a href="https://github.com/brantje/passman/wiki/Import-keepass-1-and-2" target="_blank" class="link">Read here how to export your keepass db</a><input type="file" id="importFile"/></div>').dialog({
+			buttons : {
+				"Import" : function() {
+					$('#keepassDialog').html('Importing... this may take a while')
+					setTimeout(importKeePass2,1000);
+				},
+				"Cancel" : function() {
+					$(this).dialog("close");
+
+				}
+			},
+			title : "Import password and folders from teamteampass."
+		})
+		document.getElementById('importFile').addEventListener('change', handleFileChange, false);
+	} else {
+		alert('The File APIs are not fully supported by your browser. Therefore an import is not supported');
+	}
+}
+
+/*
 function importKeepass2Dialog() {
 	// Check for the various File API support.
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
@@ -66,24 +89,32 @@ function importKeepass2Dialog() {
 		alert('The File APIs are not fully supported by your browser. Therefore an import is not supported');
 	}
 }
-
+*/
 function importKeePass2() {
 	$(document).data('importFolders',[])
-	var rows = processData(keypassData);
-	
+	var rows = processData(keepassData);
+	console.log(rows);
+	var count = 0;
 	$.each(rows, function(k, v) {
-		folder = v['group-tree']
-		var tags = folder.replace('\\',',');
+		folder = (v['group-tree']) ? v['group-tree'] : ''
+		var tags = folder.replace(/\\\\/g, ',')
+		var account =  (v['account']) ?  v['account'] : '';
+		var comments =  (v['comments']) ?  v['comments'] : '';
+		var loginName =  (v['login-name']) ?  v['login-name'] : '';
+		var password =  (v['password']) ?  v['password'] : '';
+		var webSite =  (v['web-site']) ?  v['web-site'] : '';
+		
 		var postData = {
 
-			'label' : v.account,
+			'label' : account,
 			'tags' : tags,
-			'desc' : encryptThis(v['comments']),
-			'account' : encryptThis(v['login-name']),
-			'pw1' : encryptThis(v['password']),
+			'desc' : encryptThis(comments),
+			'account' : encryptThis(loginName),
+			'pw1' : encryptThis(password),
 			'email' : encryptThis(''),
-			'url' : encryptThis(v['web-site'])
+			'url' : encryptThis(webSite)
 		}
+		console.log(postData,v);
 		var createUrl = OC.generateUrl('apps/passman/api/v1/item');
 		$.ajax({
 			async : false,
@@ -92,10 +123,12 @@ function importKeePass2() {
 			dataType : 'JSON',
 			data : postData,
 			success : function(data) {
-				console.log('Added item ' + postData.label)
-				if(k==rows.length-1){
-					KPImportDone();
-				}
+				var percent = Math.round(count/rows.length*100)
+					$('#keepassDialog').html(percent+'%...')
+					if(count==rows.length-1){
+						KPImportDone()
+					}
+					count++;
 			}
 		});
 	})
