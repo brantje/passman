4fc7eddbc82f6f2fa8dab2178b6ca54c7bf14db0
WolFi <wolfi@wolfi.es>
2014-11-04 18:57:46 +0100

    Sharing initial commit
    DB update bug "fixed"?
    DB update to fit sharing
    Added controller and manager for the shares

---
+++ db/sharemanager.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * ownCloud - passman
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Marcos Zuriaga <wolfi@wolfi.es>
+ * @copyright Marcos Zuriarga 2014
+ */
+namespace OCA\Passman\Db;
+
+use \OCP\IDb;
+use \OCP\DB\insertid;
+
+class ShareManager {
+    private $userid;
+    private $db;
+    
+    public function __construct($db) {
+            $this -> db = $db;
+    }
+    
+    
+}
+
