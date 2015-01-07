7283584f16866a3c11050399f1c2cff7e3c90595
brantje <brantje@gmail.com>
2015-01-07 15:47:17 +0100

    Update code

--- appinfo/application.php
+++ appinfo/application.php
@@ -33,7 +33,7 @@ use \OCA\Passman\Db\RevisionManager;
 
 use \OCA\PassMan\Utility\SimplePieAPIFactory;
 use \OCA\PassMan\Utility\FaviconFetcher;
-use \OCA\Passman\Service\Cronservice;
+use \OCA\Passman\Service\CronService;
 
 if (!class_exists('\SimplePie')) {
   require_once __DIR__ . '/../3rdparty/simplepie/autoloader.php';
