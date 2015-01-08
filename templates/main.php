f58cc09d0320efff46e5e50781eaa7bb21476fe8
WolFi <wolfi@wolfi.es>
2015-01-08 22:00:12 +0100

    Async and crypto wrapper

--- templates/main.php
+++ templates/main.php
@@ -20,6 +20,8 @@
 \OCP\Util::addscript('passman', 'app.directive');
 \OCP\Util::addscript('passman', 'app.filter');
 \OCP\Util::addScript('passman', 'jsrsasign-4.7.0-all-min');
+\OCP\Util::addScript('passman', 'async');
+\OCP\Util::addScript('passman', 'crypto_wrap');
 
 
 \OCP\Util::addStyle('passman', 'ocPassman');
