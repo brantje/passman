81f2c33119bcd686b4f0141fd89b2df35edfcb70
brantje <brantje@gmail.com>
2015-06-01 19:58:09 +0200

    Remove some code

--- js/app.directive.js
+++ js/app.directive.js
@@ -73,9 +73,6 @@ app.directive('progressBar', ['$rootScope',
         width:'='
       },
       template: '<div class="progress" style="width: {{width}}px;"><div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: {{percent}}%;"><span class="sr-only">{{percent}}% Complete</span></div></div>',
-      link : function(scope, element) {
-        console.log(scope)
-      }
     };
   }
 ]);
